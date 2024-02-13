b90cd90d1f90bcd14d011deb0476fd252c62ab5c
soufienaessalah <soufien.ahmedessalah@esprit.tn>
2024-02-13 23:53:31 +0000

    rankup

---
+++ src/Controller/TTTController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TTTController extends AbstractController
+{
+    #[Route('/ta', name: 'app_t_t_t')]
+    public function index(): Response
+    {
+        return $this->render('t.html.twig', [
+            'controller_name' => 'TTTController',
+        ]);
+    }
+}
